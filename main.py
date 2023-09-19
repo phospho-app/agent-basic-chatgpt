fece7a15691823d037b5f4cb9622a1d40d4f35d6
Pierre-LouisBJT <78322686+Pierre-LouisBJT@users.noreply.github.com>
2023-09-19 21:32:33 +0200

    Update

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-# main.py
-
-# Load environment variables
-
-from dotenv import load_dotenv
-load_dotenv()
-
-# use langchain to easily interact with the OpenAI API
-
 from langchain.schema import (
     AIMessage,
     HumanMessage,
     SystemMessage
 )
 from langchain.chat_models import ChatOpenAI
-     
+from phospho import Agent, Message
 
+# Load environment variables
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Define our langchain agent
 chat = ChatOpenAI(model_name="gpt-3.5-turbo",temperature=0.3)
-messages = [
-    SystemMessage(content="You are a funny and helpful assistant."),
-    HumanMessage(content="Explain to me quantum physics."),
-]
-response=chat(messages)
 
-print(response.content,end='\n')
+def my_agent(input):
+    messages = [
+        SystemMessage(content="You are a funny and helpful assistant."),
+        HumanMessage(content=input),
+    ]
+    
+    response=chat(messages)
+
+    return response.content
+
+# Package it into a phospho agent
+
+agent = Agent()
+
+@agent.chat()
+def my_chat(message):
+    # Use our agent logic to generate a response
+    response = my_agent(message.content)
+    # Return the response in a Message object
+    return Message(response)
